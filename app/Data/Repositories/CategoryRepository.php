97fb94cdb556d5d0ea477b1ee0d5f2327b45c29a
Farzana Naomi <farzananaomi@gmail.com>
2017-05-20 17:26:56 +0600

    others
    others

---
+++ app/Data/Repositories/CategoryRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hp
+ * Date: 5/20/2017
+ * Time: 5:23 PM
+ */
+
+namespace App\Data\Repositories;
+
+
+class CategoryRepository
+{
+
+}
---
+++ app/Data/Repositories/ProductRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hp
+ * Date: 5/20/2017
+ * Time: 5:24 PM
+ */
+
+namespace App\Data\Repositories;
+
+
+class ProductRepository
+{
+
+}
