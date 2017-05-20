9af5b36ebc9b8e681a343f850cf09441672c7483
Farzana Naomi <farzananaomi@gmail.com>
2017-05-20 17:39:21 +0600

    Category & Product
    Category & Product

--- app/Data/Repositories/CategoryRepository.php
+++ app/Data/Repositories/CategoryRepository.php
@@ -1,15 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hp
- * Date: 5/20/2017
- * Time: 5:23 PM
- */
 
 namespace App\Data\Repositories;
 
+use App\Data\Models\Category;
+use App\Data\Repositories\Interfaces\PaginatedResultInterface;
+use App\Data\Repositories\Interfaces\RawQueryBuilderOutputInterface;
+use App\Data\Repositories\Traits\PaginatedOutputTrait;
+use App\Data\Repositories\Traits\ProcessOutputTrait;
+use App\Data\Repositories\Traits\RawQueryBuilderOutputTrait;
 
-class CategoryRepository
+
+class CategoryRepository implements PaginatedResultInterface, RawQueryBuilderOutputInterface
 {
+    use ProcessOutputTrait, PaginatedOutputTrait, RawQueryBuilderOutputTrait;
 
+    public function search($filter = [])
+    {
+        $categories = Category::query();
+
+        return $this->output($categories);
+    }
+
+    public function find($id)
+    {
+        return Category::find($id);
+    }
+
+    public function store($data)
+    {
+        $category              = new Category();
+        $category->title = $data['title'];
+        $category->desciption  = $data['desciption'];
+      
+
+        $category->save();
+
+        return $category;
+    }
 }
--- app/Data/Repositories/ProductRepository.php
+++ app/Data/Repositories/ProductRepository.php
@@ -1,15 +1,41 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hp
- * Date: 5/20/2017
- * Time: 5:24 PM
- */
 
 namespace App\Data\Repositories;
 
+use App\Data\Models\Product;
+use App\Data\Repositories\Interfaces\PaginatedResultInterface;
+use App\Data\Repositories\Interfaces\RawQueryBuilderOutputInterface;
+use App\Data\Repositories\Traits\PaginatedOutputTrait;
+use App\Data\Repositories\Traits\ProcessOutputTrait;
+use App\Data\Repositories\Traits\RawQueryBuilderOutputTrait;
 
-class ProductRepository
+class ProductRepository implements PaginatedResultInterface, RawQueryBuilderOutputInterface
 {
+    use ProcessOutputTrait, PaginatedOutputTrait, RawQueryBuilderOutputTrait;
 
+    public function search($filter = [])
+    {
+        $products = Product::query();
+
+        return $this->output($products);
+    }
+
+    public function find($id)
+    {
+        return Product::find($id);
+    }
+
+    public function store($data)
+    {
+        $product              = new Product();
+        $product->category_id = $data['category_id'];
+        $product->Product_code  = $data['Product_code'];
+        $product->Product_name  = $data['Product_name'];
+        $product->unit_price  = $data['unit_price'];
+
+
+        $product->save();
+
+        return $product;
+    }
 }
